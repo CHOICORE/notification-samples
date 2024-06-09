12cc9563a2ce7a6b2390c861e79d759d678a02b8
CHOICORE <choijaehyeong93@gmail.com>
2024-06-09 15:57:14 +0900

    tidy: cleanup code

--- src/main/java/me/choicore/demo/notification/template/DefaultPlaceholderValidator.java
+++ src/main/java/me/choicore/demo/notification/template/DefaultPlaceholderValidator.java
@@ -52,9 +52,29 @@ public class DefaultPlaceholderValidator implements PlaceholderValidator {
         };
     }
 
-    // FIXME: 플레이스홀더 포맷을 자유도를 높게 주면 예외 상황이 발생할 것 같다.
+    // FIXME: 플레이스홀더 포맷의 자유도를 높게 주면 예외 상황이 발생할 것 같다.
     @Override
     public boolean hasPlaceholders(String template) {
+        Set<String> similarFormattedPlaceholders = getCandidateReplacingTarget(template);
+        Set<String> extractPlaceholders = formatter.extractPlaceholders(template);
+        validatePlaceholders(similarFormattedPlaceholders, extractPlaceholders);
+        return !extractPlaceholders.isEmpty();
+    }
+
+    private void validatePlaceholders(Set<String> similarFormattedPlaceholders, Set<String> extractPlaceholders) {
+        if (similarFormattedPlaceholders.size() != extractPlaceholders.size()) {
+            for (String placeholder : extractPlaceholders) {
+                placeholder = getPurePlaceholderName(formatter, placeholder);
+                similarFormattedPlaceholders.remove(placeholder);
+            }
+            String format = """
+                    Invalid placeholder '%s' detected in the template. Please use the correct format. Instead of the current prefix and suffix, use prefix '%s' and suffix '%s' respectively.
+                    """;
+            throw new IllegalArgumentException(format.formatted(String.join(",", similarFormattedPlaceholders), formatter.getPrefix(), formatter.getSuffix()));
+        }
+    }
+
+    private Set<String> getCandidateReplacingTarget(String template) {
         Set<String> expectedPlaceholders = new HashSet<>();
         for (PlaceholderFormatter candidate : embeddedCandidateFormatters) {
             candidate.getPlaceholderPattern()
@@ -66,19 +86,7 @@ public class DefaultPlaceholderValidator implements PlaceholderValidator {
                         expectedPlaceholders.add(placeholderName);
                     });
         }
-        Set<String> actual = formatter.extractPlaceholders(template);
-
-        if (expectedPlaceholders.size() != actual.size()) {
-            for (String placeholder : actual) {
-                placeholder = getPurePlaceholderName(formatter, placeholder);
-                expectedPlaceholders.remove(placeholder);
-            }
-            String format = """
-                    Invalid placeholder '%s' detected in the template. Please use the correct format. Instead of the current prefix and suffix, use prefix '%s' and suffix '%s' respectively.
-                    """;
-            throw new IllegalArgumentException(format.formatted(String.join(",", expectedPlaceholders), formatter.getPrefix(), formatter.getSuffix()));
-        }
-        return !actual.isEmpty();
+        return expectedPlaceholders;
     }
 
     private String getPurePlaceholderName(PlaceholderFormatter candidate, String placeholder) {
