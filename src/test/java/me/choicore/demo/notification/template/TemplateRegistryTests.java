2a19f8cb1ec8e9262e86c483d1eb777166cdd3b6
CHOICORE <choijaehyeong93@gmail.com>
2024-06-11 10:33:04 +0900

    tidy: delete test code that is not of interest to test

--- src/test/java/me/choicore/demo/notification/template/TemplateRegistryTests.java
+++ src/test/java/me/choicore/demo/notification/template/TemplateRegistryTests.java
@@ -18,32 +18,4 @@ class TemplateRegistryTests {
         Long savedId = registry.registerTemplate(template);
         Assertions.assertThat(savedId).isNotNull();
     }
-
-    @Test
-    void t2() {
-        String templateName = "welcome";
-        PlaceholderFormatter placeholderFormatter = new DefaultPlaceholderFormatter("#{", "}");
-
-        SubjectContentTemplate template = SubjectContentTemplate.builder()
-                .placeholderDefinition(new PlaceholderDefinition(placeholderFormatter))
-                .name(templateName)
-                .subject("Welcome to #{company}!")
-                .content("Hello, #{name}! Welcome to #{company}!")
-                .build();
-
-        Assertions.assertThat(template.hasPlaceholders()).isTrue();
-    }
-
-    @Test
-    void t3() {
-        String templateName = "welcome";
-        SubjectContentTemplate template = SubjectContentTemplate.builder()
-                .placeholderDefinition(PlaceholderDefinitions.create("#{", "}"))
-                .name(templateName)
-                .subject("Welcome to #{company}!")
-                .content("Hello, #{name}! Welcome to #{company}!")
-                .build();
-
-        Assertions.assertThat(template.hasPlaceholders()).isTrue();
-    }
 }
